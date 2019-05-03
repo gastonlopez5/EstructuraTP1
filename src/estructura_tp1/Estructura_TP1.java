8b6944acded4a223c59a5325d3f86063e30e0c2e
gaston <gaston@192.168.0.19>
2019-05-03 18:17:23 -0300

    Tercer commit - Punto 1, 2 y 3 listo

--- src/estructura_tp1/Estructura_TP1.java
+++ src/estructura_tp1/Estructura_TP1.java
@@ -5,6 +5,9 @@
  */
 package estructura_tp1;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 /**
@@ -48,56 +51,76 @@ public class Estructura_TP1 {
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
         // Punto 2
+//        Scanner sc = new Scanner(System.in);
+//        Double lados[] = new Double[3];
+//        Double lados1[] = new Double[3];
+//        int aux = 0;
+//        
+//        System.out.println("Ingrese las 3 longitudes de los lados de un triángulo: ");
+//        lados[0] = sc.nextDouble();
+//        lados[1] = sc.nextDouble();
+//        lados[2] = sc.nextDouble();
+//        
+//        lados1 = lados.clone();
+//        
+//        if ((lados[0]+lados[1] > lados[2]) && (lados[0]+lados[2] > lados[1]) && (lados[2]+lados[1] > lados[0])){
+//            
+//            for (int j=0; j<lados.length; j++){
+//                double a = lados[j];
+//                for (int i=0; i<lados.length; i++){
+//                    double b = lados1[i];
+//                    if (a == b){
+//                        aux += 1;
+//                    }
+//                }
+//            }
+//            
+//            System.out.println("aux= " + aux);
+//            
+//            switch (aux){
+//                case 3:
+//                    System.out.println("Es un triangulo escaleno");
+//                    aux = 0;
+//                    break;
+//                    
+//                case 5:
+//                    System.out.println("Es un triangulo isoceles");
+//                    aux = 0;
+//                    break;
+//                
+//                case 9:
+//                    System.out.println("Es un triangulo equilátero");
+//                    aux = 0;
+//                    break;
+//                
+//                default:
+//                    break;
+//            }
+//        
+//        }else{
+//            System.out.println("No es posible formar un triángulo con los tres lados ingresados");
+//        } 
+//        
+//    }
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        // Punto 3
         Scanner sc = new Scanner(System.in);
-        Double lados[] = new Double[3];
-        Double lados1[] = new Double[3];
-        int aux = 0;
+        LocalDate fechaNacimiento;
+        LocalDate fechaActual = LocalDate.now();
+        int edad;
         
-        System.out.println("Ingrese las 3 longitudes de los lados de un triángulo: ");
-        lados[0] = sc.nextDouble();
-        lados[1] = sc.nextDouble();
-        lados[2] = sc.nextDouble();
+        try{
+            System.out.println("Ingresar fecha de nacimiento dd/mm/aaaa: ");
+            String fecha = sc.next();
+            fechaNacimiento = LocalDate.parse(fecha, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         
-        lados1 = lados.clone();
+            edad = fechaActual.getYear() - fechaNacimiento.getYear();
+            System.out.println("Usted tiene: "+ edad);
+        }catch (DateTimeParseException ex){
+            System.out.println("Error en el formato con el que ingresó la fecha.");
+        }
         
-        if ((lados[0]+lados[1] > lados[2]) && (lados[0]+lados[2] > lados[1]) && (lados[2]+lados[1] > lados[0])){
-            
-            for (int j=0; j<lados.length; j++){
-                double a = lados[j];
-                for (int i=0; i<lados.length; i++){
-                    double b = lados1[i];
-                    if (a == b){
-                        aux += 1;
-                    }
-                }
-            }
-            
-            System.out.println("aux= " + aux);
-            
-            switch (aux){
-                case 3:
-                    System.out.println("Es un triangulo escaleno");
-                    aux = 0;
-                    break;
-                    
-                case 5:
-                    System.out.println("Es un triangulo isoceles");
-                    aux = 0;
-                    break;
-                
-                case 9:
-                    System.out.println("Es un triangulo equilátero");
-                    aux = 0;
-                    break;
-                
-                default:
-                    break;
-            }
-        
-        }else{
-            System.out.println("No es posible formar un triángulo con los tres lados ingresados");
-        } 
-        
-    }
-    
+        }
 }
